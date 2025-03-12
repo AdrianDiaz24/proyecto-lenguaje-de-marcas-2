5589528699c785e2dd53a8475065ee1149e23138
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2025-03-12 01:31:31 +0100

    añadir script.js para cambio de modo oscuro y mensaje cuando se activa o desactiva

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+
+
+const darkModeToggle = document.getElementById('dark-mode');
+const body = document.body;
+const footer = document.querySelector('footer');
+
+darkModeToggle.addEventListener('change', () => {
+    console.log('Checkbox cambiado:', darkModeToggle.checked);
+
+    if (darkModeToggle.checked) {
+
+        body.setAttribute('data-theme', 'dark-mode');
+        footer.setAttribute('data-theme', 'dark-mode-footer');
+        const message = document.createElement('div');
+        message.textContent = '🌙 Modo oscuro activado';
+        message.style.position = 'fixed';
+        message.style.bottom = '10px';
+        message.style.right = '10px';
+        message.style.backgroundColor = '#000';
+        message.style.color = '#fff';
+        message.style.padding = '10px';
+        message.style.borderRadius = '5px';
+        document.body.appendChild(message);
+
+        setTimeout(() => {
+            document.body.removeChild(message);
+        }, 1000);
+        
+    } else {
+
+        body.removeAttribute('data-theme');
+        footer.removeAttribute('data-theme');
+        const message = document.createElement('div');
+        message.textContent = '☀️ Modo oscuro desactivado';
+        message.style.position = 'fixed';
+        message.style.bottom = '10px';
+        message.style.right = '10px';
+        message.style.backgroundColor = '#000';
+        message.style.color = '#fff';
+        message.style.padding = '10px';
+        message.style.borderRadius = '5px';
+        document.body.appendChild(message);
+
+        setTimeout(() => {
+            document.body.removeChild(message);
+        }, 1000);
+    }
+
+    console.log('Atributo body:', body.getAttribute('data-theme'));
+    console.log('Atributo footer:', footer.getAttribute('data-theme'));
+});
