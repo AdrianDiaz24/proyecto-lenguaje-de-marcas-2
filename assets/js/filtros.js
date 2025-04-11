275b8a8022e3d92d284f8e4031de77dac336cb83
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2025-04-11 11:24:08 +0200

    Añadir filtros noticias software

--- assets/js/filtros.js
+++ assets/js/filtros.js
@@ -1,5 +1,7 @@
 const filtrosHardware = document.querySelectorAll('input[name="filtroHardware"]');
 const articulosHardware = document.getElementById('noticiasHardware').querySelectorAll('div > div');
+const filtrosSoftware = document.querySelectorAll('input[name="filtroSoftware"]');
+const articulosSoftware = document.getElementById('noticiasSoftware').querySelectorAll('div > div');
 
 filtrosHardware.forEach(filtro => {
   filtro.addEventListener('change', () => {
@@ -16,5 +18,24 @@ filtrosHardware.forEach(filtro => {
         articulo.style.display = 'none';
       }
     };
+  });
+});
+
+
+filtrosSoftware.forEach(filtro => {
+  filtro.addEventListener('change', () => {
+    const filtrosActivosSoftware = Array.from(filtrosSoftware)
+      .filter(f => f.checked)
+      .map(f => f.value);
+
+    articulosSoftware.forEach(articulo => {
+      const categoria = articulo.id;  
+
+      if (filtrosActivosSoftware.length === 0 || filtrosActivosSoftware.includes(categoria)) {
+        articulo.style.display = 'block';
+      } else {
+        articulo.style.display = 'none';
+      }
+    });
   });
 });
